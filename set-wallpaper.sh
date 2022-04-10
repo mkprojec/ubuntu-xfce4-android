87b47b0e59953c15d59cfa2de911a5e741b8dcec
mkprojec <101967322+mkprojec@users.noreply.github.com>
2022-04-11 06:17:15 +0700

    Delete set-wallpaper.sh

--- set-wallpaper.sh
+++
@@ -1,3 +0,0 @@
-xfconf-query --channel xfce4-desktop --list | grep last-image | while read path; do
-    xfconf-query --channel xfce4-desktop --property $path --set /usr/share/endeavouros/endeavouros-wallpaper.png
-done
